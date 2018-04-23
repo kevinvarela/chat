67b1ef58f2c60f8e8d4f64290d8742e80db48cd0
kevinvarela <kevin.varela@despegar.com>
2018-04-23 18:40:26 -0300

    persistence
    Signed-off-by: kevinvarela <kevin.varela@despegar.com>

--- index.js
+++ index.js
@@ -17,27 +17,23 @@ app.get('/', function(req, res){
 });
 
 app.get('/hm', function(req, res){
-  // request.get(urlDB, function(err, httpResponse, body) {
-  //   historyMessages = JSON.parse(body);
-  // })
-  let dataString = fs.readFileSync('dataBase.json');
-  historyMessages = JSON.parse(dataString);
+  request.get(urlDB, function(err, httpResponse, body) {
+    historyMessages = JSON.parse(body);
+  })
   res.send(historyMessages);
 });
 
 app.get('/resetK', function(req, res){
   var historyMessagesReset = {'messages':["Bienvenido a KVA-CHAT 1.3..."]};
-  // request({url:urlDB, method:'PUT', json: historyMessagesReset}, function(request, response){
-    // console.log("Method PUT: 'Reset DB'");
-    fs.writeFile("dataBase.json", JSON.stringify(historyMessagesReset));
-    console.log("-- Reset DB --");
+  request({url:urlDB, method:'PUT', json: historyMessagesReset}, function(request, response){
+    console.log("Method PUT: 'Reset DB'");
     res.send("History reset")
-  // })
+  })
 });
 
 io.on('connection', function(socket){
   usersConnected += 1;
-  console.log("Users Online = " + usersConnected);
+  console.log("Users Online = " + usersConnected + ' | Id: '+ socket.id);
   socket.on('disconnect', function(){
     usersConnected -= 1;
     console.log("Users Online = " + usersConnected);
@@ -46,7 +42,7 @@ io.on('connection', function(socket){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    console.log('message: ' + msg + ' | Id: '+ socket.id);
   });
 });
 
@@ -60,13 +56,12 @@ io.on('connection', function(socket){
 var updateDB = function(msg){
   historyMessages.messages.push(msg);
   var historialLength = historyMessages.messages.length;
-  console.log(historialLength);
   if( historialLength >= LIMITE_DE_MENSAJES){
     historyMessages.messages = historyMessages.messages.slice(historialLength - LIMITE_DE_MENSAJES)
     console.log("Slice messages to: "+ historyMessages.messages.length);
   }
-  // request({url:urlDB, method:'PUT', json: historyMessages}, function(request, response){
-    fs.writeFile("dataBase.json", JSON.stringify(historyMessages));
+  request({url:urlDB, method:'PUT', json: historyMessages}, function(request, response){
+  })
 }
 
 var port = process.env.PORT || 3000;
